41672504521d6dad33ac5077669a1a2703b7f17b
Veronica Williams <williamsv4@students.rowan.edu>
2017-10-12 00:01:19 -0400

    finished code for all boards

--- Button Interrupt/MSP430G2553/ButtonInterrupt/main.c
+++ Button Interrupt/MSP430G2553/ButtonInterrupt/main.c
@@ -1,6 +1,6 @@
 #include <msp430.h>
 
-#define LED_0 BIT0
+#define LED_0 BIT0 //rename variables for easier coding
 #define LED_1 BIT6
 #define LED_OUT P1OUT
 #define LED_DIR P1DIR
@@ -9,28 +9,34 @@ unsigned int blink = 0;
 void main(void)
 {
 WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
-LED_DIR |= (LED_0 + LED_1); // Set P1.0 and P1.6 to output direction
+LED_DIR |= (LED_0 + LED_1); // Set P1.0 and P1.6 to outputs
 LED_OUT &= ~(LED_0 + LED_1); // Set the LEDs off
-P1REN |= BUTTON; //Enables a puller-Resistor on the button-pin
-P1OUT |= BUTTON; //Writes a "1" to the portpin, tellling the resistor to pullup
-P1IE |= BUTTON; //Enables the selector-mask for generating interrupts on the relevant pin
-__enable_interrupt(); // Interrupts get enabled *here* - they were disabled thus far..
-for (;;)
+P1REN |= BUTTON; //Enables pullup or pulldown resistor
+P1OUT |= BUTTON; //pullup resistor selected
+P1IE |= BUTTON; //enable interrupt for pin 1.3
+__enable_interrupt(); // interrupts are enabled
+for (;;)//infinite loop
 {
-if(blink > 0)
+if(blink > 0) //activated when the button is pressed
 {
-P1OUT ^= (LED_0 + LED_1); // Toggle P1.0 and P1.6 using exclusive-OR
-__delay_cycles(100000); // SW Delay of 10000 cycles at 1Mhz
+P1OUT ^= (LED_0 + LED_1); // Toggle P1.0 and P1.6 
+__delay_cycles(100000); // sets delay
 }
 }
 }
 // Port 1 interrupt service routine
-#pragma vector=PORT1_VECTOR
+#pragma vector=PORT1_VECTOR //start interrupr
 __interrupt void Port_1(void)
 {
-blink ^= 0x01;
-P1IFG &= ~BUTTON; // P1.3 IFG cleared
-LED_OUT &= ~(LED_0 + LED_1); // Clear the LEDs so they start in OFF state
-P1IES ^= BUTTON;
+blink ^= 0x01; //toggle blink to 1, goes to for loop
+P1IFG &= ~BUTTON; // clear flag
+LED_OUT &= ~(LED_0 + LED_1); // turn LEDs off
+P1IES ^= BUTTON; //toggle flag to rising edge
 }
-
+//Since the flag is set to rising edge, when the button is released: 
+// - the flag is set to 1, triggering the interrupt again
+// - blink = 0
+// - the flag is turned off
+// - LEDs are turned off
+// - flag is set to rising edge
+// - interrupt will not be triggered again until the button is pressed
